8f9143997d857b34fd8ab7bb6db36ca74ddd8e14
aniversarioperu <aniversarioperu1@gmail.com>
2014-06-14 14:15:17 +0300

    la punta

--- extract_district.py
+++ extract_district.py
@@ -1,25 +1,27 @@
 #-*- coding: utf-8 -*-
 import json
-import sys
 
 
-names = ["LIMA", "SAN MARTIN DE PORRES", "ANCON", "ATE", "BARRANCO", u"BREÑA",
-        "CARABAYLLO", "CHACLACAYO", "CHORRILLOS", "CIENEGUILLA", "COMAS",
-        "EL AGUSTINO", "INDEPENDENCIA", "JESUS MARIA", "LA MOLINA",
-        "LA VICTORIA", "LINCE", "LOS OLIVOS", "LURIGANCHO", "LURIN", 
-        "MAGDALENA DEL MAR", "MIRAFLORES", "PACHACAMAC", "PUCUSANA", 
-        "PUEBLO LIBRE", "PUENTE PIEDRA", "PUNTA HERMOSA", "PUNTA NEGRA",
-        "RIMAC", "SAN BARTOLO", "SAN BORJA", "SAN ISIDRO", 
-        "SAN JUAN DE LURIGANCHO", "SAN LUIS", "SAN MIGUEL", "SANTA ANITA",
-        "SANTA ROSA", "SANTIAGO DE SURCO", "SURQUILLO", "VILLA EL SALVADOR",
-        "VILLA MARIA DEL TRIUNFO"]
+names = [
+    "LIMA", "ANCON", "ATE", "BARRANCO", u"BREÑA", "CARABAYLLO",
+    "CHACLACAYO", "CHORRILLOS", "CIENEGUILLA", "COMAS", "EL AGUSTINO",
+    "INDEPENDENCIA", "JESUS MARIA", "LA MOLINA", "LA VICTORIA", "LINCE",
+    "LOS OLIVOS", "LURIGANCHO", "LURIN", "MAGDALENA DEL MAR", 
+    "MAGDALENA VIEJA", "MIRAFLORES",
+    "PACHACAMAC", "PUCUSANA", "PUEBLO LIBRE", "PUENTE PIEDRA", "PUNTA HERMOSA",
+    "PUNTA NEGRA", "RIMAC", "SAN BARTOLO", "SAN BORJA", "SAN ISIDRO",
+    "SAN JUAN DE LURIGANCHO", "SAN MARTIN DE PORRES",
+    "SAN JUAN DE MIRAFLORES", "SAN LUIS", "SAN MIGUEL",
+    "SANTA ANITA", "SANTA ROSA", "SANTIAGO DE SURCO", "SURQUILLO", 
+    "VILLA EL SALVADOR", "VILLA MARIA DEL TRIUNFO",
+    "BELLAVISTA", "LA PERLA", "CALLAO", "VENTANILLA", 
+    "CARMEN DE LA LEGUA REYNOSO", "LA PUNTA",
+]
 
-if len(sys.argv) < 2:
-    print "Enter ubigeo as argument (only three first digits)"
-    sys.exit(0)
 
+ubigeos = ["1501", "0701"]
 print "Districts are harcoded in list ``names``", names
-ubigeo = str(sys.argv[1].strip())
+print "Ubigeos are harcoded in list ``ubigeos`` (only first 4 digits)", ubigeos
 
 districts = json.loads(open("districts_peru.geojson", "r").read())
 
@@ -31,7 +33,7 @@ for  i in districts['features']:
     for k, v in i.items():
         if i['properties']['name'] in names \
                 and i['properties']['id'] not in ids \
-                and i['properties']['ubigeo'].startswith(ubigeo):
+                and i['properties']['ubigeo'][0:4] in ubigeos:
             objects.append(i)
             ids.append(i['properties']['id'])
 
